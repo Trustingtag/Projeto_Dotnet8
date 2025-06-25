63e3ef93b1bd541d561d5da5a9a659bdfeaced59
leonardo <leonardo.a.oliveira6@ba.estudante.senai.br>
2025-06-25 16:44:55 -0300

    Vr8-Leo

---
+++ Controllers/PrincipalController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Projeto_Dotnet8.Models;
+using Projeto_Dotnet8.Repository;
+namespace Reserva.Controllers;
+
+public class Principal : Controller
+{
+    private readonly ILogger<Principal> _logger;
+
+    public Principal(ILogger<Principal> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
