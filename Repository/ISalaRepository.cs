e8e93a913687b4f6df5116a6df13eeb648bf570d
Spcmp1 <leonardo.a.oliveira6@ba.estudante.senai.br>
2025-06-25 09:22:45 -0300

    ProjetoDN8_DS_Vr4

---
+++ Repository/ISalaRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Dotnet8.Repository
+{
+    public interface ISalaRepository
+    {
+        
+    }
+}
