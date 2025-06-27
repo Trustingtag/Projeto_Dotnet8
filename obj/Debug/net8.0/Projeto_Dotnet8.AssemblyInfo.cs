6c4bab9eb8a542351d3e0c353eb10f68d9a10faa
Ryan-Marcel <ryan.m.santos@ba.estudante.senai.br>
2025-06-27 16:15:40 -0300

    VR FINAL FINAL

--- obj/Debug/net8.0/Projeto_Dotnet8.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto_Dotnet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Dotnet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb1d608772e74de85ae8b637887105fdaa7c0e55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd891f9518067428bf7700fba93569843c524a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Dotnet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Dotnet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
